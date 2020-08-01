57473e463281e5e08a34d2d145860aeb29e7c8da
mexell96 <mexell96@gmail.com>
2020-08-01 10:56:27 +0300

    added skill and colors

--- src/Site.js
+++ src/Site.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Site ({number}) {
-  return <div>Counter {number > 5 ? ":)" : number}</div>;
+    if (number > 0 && number <=4) {
+        return <div className="green">Counter: {number} Без опыта</div>;
+    }
+    else if (number > 4 && number <=9) {
+        return <div className="grey">Counter: {number} Новичёк</div>;
+    }
+    else if (number > 9 && number <=14) {
+        return <div className="yellow">Counter: {number} Мастер</div>;
+    }
+    else if (number > 14) {
+        return <div className="violet">Counter: {number} Профи</div>;
+    }
+    return <div>Counter: N/A</div>
 }
